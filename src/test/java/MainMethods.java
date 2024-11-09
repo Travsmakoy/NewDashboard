4ce3c5e33e32679cac6c3b7eacd6b1f4d2b2349d
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-11-09 12:06:12 +0400

    Test!

--- src/test/java/MainMethods.java
+++ src/test/java/MainMethods.java
@@ -1,15 +1,12 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.ArrayList;
 
