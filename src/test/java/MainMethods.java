8477d88ffbde0ed32cd314e00b34020a7a0f73a9
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-11-09 10:43:35 +0400

    first commit

---
+++ src/test/java/MainMethods.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
+
+public class MainMethods {
+    protected static WebDriverWait wait;
+    protected static WebDriver driver;
+
+    @BeforeSuite
+    public void setUpSuite() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+    @AfterSuite
+    public void tearDownSuite() throws InterruptedException {
+        // Quit the WebDriver instance
+        if (driver != null) {
+            Thread.sleep(2000);
+            driver.quit();
+            System.out.println("TEST DONE");
+        }
+    }
+    public void click(By locator){
+        WebElement click = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        click.click();
+    }
+    public void URLvalidator(String value){
+        Assert.assertEquals(driver.getCurrentUrl(),value);
+        System.out.println("You are in correct URL "+value);
+    }
+}
---
+++ src/test/java/Start.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Start extends MainMethods{
+    @Test
+    public void VerifyAqarySite(){
+        driver.get("http://192.168.1.138:8080");
+        URLvalidator("http://192.168.1.138:8080/");
+        click(By.xpath("/html/body/div/a/button"));
+        URLvalidator("http://192.168.1.138:8080/login?next=http%3A%2F%2Flocalhost%3A8080%2Fdashboard");
+    }
+}
