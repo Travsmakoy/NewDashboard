4a39614c4f4314eb70e181665be23a8a660733e0
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-11-09 11:50:28 +0400

    Test

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -14,13 +14,11 @@ public class Login extends MainMethods {
     }
     @Test (priority = 3)
     public void clear() throws InterruptedException {
-        Thread.sleep(1000);
         clearfield(By.xpath("(//input[@id=':r0:'])[1]"));
         clearfield(By.xpath("(//input[@id=':r1:'])[1]"));
     }
     @Test(priority = 4)
     public void EnterValidUser() throws InterruptedException {
-        Thread.sleep(1000);
         clickandsend(By.xpath("//input[@id=\":r0:\"]"),"mark@admin.com");
         clickandsend(By.xpath("//input[@id=\":r1:\"]"),"mark");
         click(By.xpath("//button[normalize-space()=\"Sign In\"]"));
