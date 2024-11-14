81356728acd7c17a2d269c6fbb45cacfccd65abc
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-11-14 17:46:33 +0400

    Used Placeholder for Dropdowns. and Class MUILIST

--- src/test/java/MainMethods.java
+++ src/test/java/MainMethods.java
@@ -2,6 +2,8 @@ import io.qameta.allure.Allure;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,7 +20,7 @@ public class MainMethods {
 
     @BeforeSuite
     public void setUpSuite() {
-        driver = new FirefoxDriver();
+        driver = new EdgeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
--- src/test/java/addUser.java
+++ src/test/java/addUser.java
@@ -7,5 +7,7 @@ public class addUser extends MainMethods{
         click(By.xpath("//button[@aria-label=\"open drawer\"]//*[name()=\"svg\"]"));
         click(By.xpath("/html/body/div[2]/div/div/nav[2]/div[1]"));
         click(By.xpath("/html/body/div[2]/div/div/nav[2]/div[2]/div/div/a/div"));
+        clickandsend(By.xpath("//input[@placeholder='Enter username']"),"Marko");
+        clickandsend(By.xpath("//input[@placeholder='Enter first name']"),"Marko");
     }
 }
