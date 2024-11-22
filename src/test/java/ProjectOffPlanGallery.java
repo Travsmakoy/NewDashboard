72b040860de8ea7a5f35ebbcb67d44c59d9ea607
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-11-22 18:10:34 +0400

    refactored

--- src/test/java/ProjectOffPlanGallery.java
+++ src/test/java/ProjectOffPlanGallery.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class ProjectOffPlanGallery extends MainMethods {
@@ -9,6 +10,8 @@ public class ProjectOffPlanGallery extends MainMethods {
         click(By.xpath("//button[normalize-space()=\"Add Gallery\"]"));
         doubleClick(By.xpath("//input[@placeholder='Select Gallery Type']"),By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and position()=1]"));
         doubleClick(By.xpath("//input[@placeholder='Select Media Type']"),By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and position()=1]"));
-        click(By.xpath("//*[@id=\":rck:\"]"));
+        WebElement fileInput = driver.findElement(By.cssSelector("MuiInputBase-root"));
+        fileInput.sendKeys("\"D:\\Mark OneDrive\\OneDrive - aqary international group\\Desktop\\1730355295352.jpg\"");
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div/div[1]/form/div[4]/button[1]")).click();
     }
 }
