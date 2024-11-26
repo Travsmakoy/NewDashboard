eafa68cef772960decec03b326c89c5948a517f9
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-11-26 15:56:45 +0400

    refactored

--- src/test/java/Company.java
+++ src/test/java/Company.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Company extends MainMethods {
@@ -33,5 +36,27 @@ public class Company extends MainMethods {
         doubleClick(By.xpath("//input[@placeholder='Select Community']"),By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and position()="+randomCom+"]"));
         doubleClick(By.xpath("//input[@placeholder='Select Sub Community']"),By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and position()="+randomSub+"]"));
     }
+    @Test(priority = 4)
+    public void AddDetails() throws InterruptedException {
+        scrollPage(500);
+        clickandsend(By.name("commercial_license_no"),"CommercialLicense"+randomint4);
 
+        String folderPath = "D:\\Mark OneDrive\\OneDrive - aqary international group\\Desktop\\IMAGES";
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+
+        if (files == null || files.length == 0) {
+            System.out.println("No files found in the folder!");
+            return;
+        }
+
+        Random random = new Random();
+        File randomFile = files[random.nextInt(files.length)];
+        String filePath = randomFile.getAbsolutePath();
+
+        WebElement fileInput = driver.findElement(By.xpath("//input[@type=\"file\"]"));
+        fileInput.sendKeys(filePath);
+
+        Thread.sleep(200);
+    }
 }
