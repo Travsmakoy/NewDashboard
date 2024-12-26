baf4351d2c16142e136632e9599bcc62f9348d44
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-12-26 14:15:11 +0400

    changed the validation

--- src/test/java/PropertyHubGal.java
+++ src/test/java/PropertyHubGal.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
-public class PropertyHubGal {
+import java.io.File;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class PropertyHubGal extends MainMethods {
+    @Test(priority = 1)
+    public void NavToProjectPropertyGallery(){
+        click(By.xpath("/html/body/div[2]/main/div/div/div[2]/div/div[2]/table/tbody/tr[1]/td[16]/div/button[6]"));
+        click(By.xpath("//*[@id=\"actions-popover\"]/div[3]/div[2]/a"));
+    }
+    @Test(priority = 2)
+    public void AddProjectPropertyGallery() throws InterruptedException {
+        for (int i = 0; i < 7; i++) {
+            click(By.xpath("/html/body/div[2]/main/div/div/div[2]/div/div[1]/div[3]/div[1]/button[1]"));
+            String randomint = String.valueOf(ThreadLocalRandom.current().nextInt(1, 6));
+            doubleClick(By.xpath("//input[@placeholder='Select Gallery Type']"), By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and position()=" + randomint + "]"));
+
+            doubleClick(By.xpath("//input[@placeholder='Select Media Type']"), By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and position()=1]"));
+
+            String folderPath = "D:\\Mark OneDrive\\OneDrive - aqary international group\\Desktop\\IMAGES";
+            File folder = new File(folderPath);
+            File[] files = folder.listFiles();
+
+            if (files == null || files.length == 0) {
+                System.out.println("No files found in the folder!");
+                return;
+            }
+
+            Random random = new Random();
+            File randomFile = files[random.nextInt(files.length)];
+            String filePath = randomFile.getAbsolutePath();
+
+            WebElement fileInput = driver.findElement(By.xpath("//input[@type=\"file\"]"));
+            fileInput.sendKeys(filePath);
+
+            click(By.xpath("/html/body/div[3]/div[3]/div/div/div[1]/form/div[4]/button[1]"));
+
+
+            Thread.sleep(200);
+        }
+        click(By.xpath("/html/body/div[2]/main/div/div/div[1]/div/a"));
+    }
 }
--- src/test/java/PropertyHubPaymentPlan.java
+++ src/test/java/PropertyHubPaymentPlan.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
-public class PropertyHubPaymentPlan {
+public class PropertyHubPaymentPlan extends MainMethods{
+    @Test(priority = 1)
+    public void GotoPayment(){
+       click(By.xpath("/html/body/div[2]/main/div/div/div[2]/div/div[2]/table/tbody/tr[1]/td[16]/div/button[6]"));
+       click(By.xpath("//*[@id=\"actions-popover\"]/div[3]/div[4]/a"));
+    }
+    @Test(priority = 2)
+    public void AddDetails() throws InterruptedException {
+        click(By.xpath("/html/body/div[2]/main/div/div/div[2]/div/div[1]/a"));
+        Thread.sleep(350);
+        clickandsend(By.xpath("//input[@placeholder='Select Percentage' and @name='plans.0.payments.0.percentage']"),"100");
+        doubleClick(By.xpath("/html/body/div[2]/main/form/div[1]/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div/div[2]/button"),By.xpath("/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/button[5]"));
+        clickandsend(By.name("plans.0.payments.0.milestone"),"THIS IS RANDOM TEST DATA");
+        click(By.xpath("/html/body/div[2]/main/form/div[2]/button[1]"));
+        click(By.xpath("/html/body/div[2]/main/form/div[2]/button[1]"));
+        click(By.xpath("/html/body/div[2]/main/form/div/div/div[2]/div/div[4]/div/div[2]/a"));
+
+    }
 }
