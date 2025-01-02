7ece395791e0975715f44c19c797ebe3c24d07ff
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2025-01-02 11:18:44 +0400

    latest development

--- src/test/java/RUNNER.java
+++ src/test/java/RUNNER.java
@@ -4,11 +4,11 @@ import java.util.Collections;
 public class RUNNER {
     public static void main(String[] args) {
         TestNG testng = new TestNG();
-        testng.setTestSuites(Collections.singletonList("src/test/java/WithoutAProject.xml"));
+        testng.setTestSuites(Collections.singletonList("src/test/java/ProjectReady.xml"));
         int iterations = 5; // Number of times to run the suite
 
         for (int i = 0; i < iterations; i++) {
-            System.out.println("Running TestNG suite - iteration: " + (i + 1));
+            System.out.println("Running TestNG suite - iteration: " + (i + 2));
             testng.run();
         }
     }
